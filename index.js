7de5884f2e8bc98a1b4447c11c6be9048edb1499
Sun-Mountain <npzonnenberg@gmail.com>
2019-05-24 09:41:07 -0400

    observables

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ console.log('the javascript is running... catch it!')
 
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.firstName = "Nicole";
-    this.lastName = "Zonnenberg";
+    this.firstName = ko.observable("Nicole");
+    this.lastName = ko.observable("Zonnenberg");
+
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();
+    }, this);
+
 }
 
 // Activates knockout.js
